c4e79712ee20d08509db47977cf2a2d4f9fd6d89
YA <YA-androidapp@users.noreply.github.com>
2022-03-18 12:07:03 +0900

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2019 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+# Copyright (c) 2022 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
 
 USER='ya-androidapp'
 
@@ -9,7 +9,7 @@ fi
 
 for page in $(seq 1 10)
 do
-REPONAME=$(curl -s "https://api.github.com/users/$USER/repos?page=$page&per_page=100"|grep "\"full_name\""|cut -d'"' -f4)
+REPONAME=$(curl -s "https://api.github.com/users/$USER/repos?page=$page&per_page=100"|jq '.[] | select(.size < 100000)'|grep "\"full_name\""|cut -d'"' -f4)
 if [ -z "$REPONAME" ]; then break;fi
 echo "$REPONAME"|sed -e "s~${USER}/~~ig" >> result.txt
 done
